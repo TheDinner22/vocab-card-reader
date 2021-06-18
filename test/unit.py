1dcebbea16ef8e1cbfaf8a5687241814567fb729
Joseph Goodman <jgood1133@gmail.com>
2021-06-18 17:07:17 -0400

    🧪 - added 2 unit tests for Reader.text_to_speech(), both are meant to pass

--- test/unit.py
+++ test/unit.py
@@ -6,7 +6,54 @@ import os, sys
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # adds project dir to places it looks for the modules
 sys.path.append(BASE_PATH)
 
+from lib.text_to_speech import Reader
 
+# Reader.text_to_speech() should not throw when passed any data-type
+def test1(done):
+    # first create all the data types
+    example_list = ["i","am","example"]
+    example_dict = {"testing right now": True, "happy": False, "am i cool?" : 100}
+    def example_func():
+        pass
+    class Getting_out_of_hand():
+        do_i_really_need_to_be_testing_it_with_a_class = "nope"
+        so_why = "is funny"
+    all_bad_data_types = ["stringy boi", 123, 123.123, True, False, example_list, example_dict, example_func, Getting_out_of_hand, None]
+
+    # loop through every data type, making sure the function does not throw
+    reader = Reader(use_test_dir=True)
+    for data_type in all_bad_data_types:
+        try:
+            reader.text_to_speech(data_type)
+        except Exception as e:
+            e = str(e)
+            msg = "reader.text_to_speech threw when it was passed:\n" + str(data_type) + "\n gave error msg:\n" + e
+            raise AssertionError(msg)
+    #if no exceptions were raised during the loop, the test is a pass
+    done("Reader.text_to_speech() should not throw when passed any data-type")
+unit_tests["Reader.text_to_speech() should not throw when passed any data-type"] = test1
+
+def test2(done):
+    # define input and expected output
+    test_input = "testing hurts my neck"
+    expected_outcome_path = "data/test/testing_hurts_my_neck.mp3"
+
+    # create the reader and run with the test input
+    reader = Reader(use_test_dir=True)
+    reader.text_to_speech(test_input)
+
+    # check to see if the file was created
+    if os.path.exists(expected_outcome_path):
+        # remove the file so the test can be ran multiple times
+        os.remove(expected_outcome_path)
+
+        # the file was created so the test passes
+        done("Reader.text_to_speech() should create a file in the data/test dir")
+    else:
+        raise AssertionError("file: " + expected_outcome_path + " was not found")
+unit_tests["Reader.text_to_speech() should create a file in the data/test dir"] = test2
+
+"""
 # example tests
 def one_plus_one_is_two(done):
     outcome = 1+1
@@ -21,3 +68,4 @@ def one_plus_one_is_three(done):
     assert outcome == desired_outcome, "1+1 was not equal to three"
     done("one plus one is equal to three")
 unit_tests["one plus one is equal to three"] = one_plus_one_is_three
+"""
