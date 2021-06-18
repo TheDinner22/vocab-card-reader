26afb91af793470288f8fee269da6cf4aedfe639
Joseph Goodman <jgood1133@gmail.com>
2021-06-18 18:06:33 -0400

    ❌ - parse_and_play works but its unit test fails, i have no idea what is causing this error. i cannot recreate the error, so i think it is a problem with the test

--- test/unit.py
+++ test/unit.py
@@ -78,6 +78,33 @@ def test3(done):
     done("Reader.play_text() should not throw when passed any data-type")
 unit_tests["Reader.play_text() should not throw when passed any data-type"] = test3
 
+# Reader.parse_and_play() should not throw when passed any data-type
+def test4(done):
+    # first create all the data types
+    example_list = ["i","am","example"]
+    example_dict = {"testing right now": True, "happy": False, "am i cool?" : 100}
+    def example_func():
+        pass
+    class Getting_out_of_hand():
+        do_i_really_need_to_be_testing_it_with_a_class = "nope"
+        so_why = "is funny"
+    all_bad_data_types = ["stringy boi", 123, 123.123, True, False, example_list, example_dict, example_func, Getting_out_of_hand, None]
+
+    all_bad_data_types = ["stringy boi"]
+
+    # loop through every data type, making sure the function does not throw
+    reader = Reader(use_test_dir=True)
+    for data_type in all_bad_data_types:
+        try:
+            reader.parse_and_play(data_type)
+        except Exception as e:
+            e = str(e)
+            msg = "reader.parse_and_play() threw when it was passed:\n" + str(data_type) + "\n gave error msg:\n" + e
+            raise AssertionError(msg)
+    #if no exceptions were raised during the loop, the test is a pass
+    done("Reader.parse_and_play() should not throw when passed any data-type")
+unit_tests["Reader.parse_and_play() should not throw when passed any data-type"] = test4
+
 """
 # example tests
 def one_plus_one_is_two(done):
