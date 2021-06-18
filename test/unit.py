22fca88189394ca414454ad2b08752adcd180c09
Joseph Goodman <jgood1133@gmail.com>
2021-06-18 17:39:52 -0400

    🧪 - added a unit test for the Reader.play_text() function

--- test/unit.py
+++ test/unit.py
@@ -53,6 +53,31 @@ def test2(done):
         raise AssertionError("file: " + expected_outcome_path + " was not found")
 unit_tests["Reader.text_to_speech() should create a file in the data/test dir"] = test2
 
+# Reader.play_text() should not throw when passed any data-type
+def test3(done):
+    # first create all the data types
+    example_list = ["i","am","example"]
+    example_dict = {"testing right now": True, "happy": False, "am i cool?" : 100}
+    def example_func():
+        pass
+    class Getting_out_of_hand():
+        do_i_really_need_to_be_testing_it_with_a_class = "nope"
+        so_why = "is funny"
+    all_bad_data_types = ["stringy boi", 123, 123.123, True, False, example_list, example_dict, example_func, Getting_out_of_hand, None]
+
+    # loop through every data type, making sure the function does not throw
+    reader = Reader(use_test_dir=True)
+    for data_type in all_bad_data_types:
+        try:
+            reader.play_text(data_type)
+        except Exception as e:
+            e = str(e)
+            msg = "reader.play_text() threw when it was passed:\n" + str(data_type) + "\n gave error msg:\n" + e
+            raise AssertionError(msg)
+    #if no exceptions were raised during the loop, the test is a pass
+    done("Reader.play_text() should not throw when passed any data-type")
+unit_tests["Reader.play_text() should not throw when passed any data-type"] = test3
+
 """
 # example tests
 def one_plus_one_is_two(done):
