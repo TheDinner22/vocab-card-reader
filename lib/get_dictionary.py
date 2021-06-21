8922e669d3bcad9fbe3dc54ff44dcab2c3865b04
Joseph Goodman <jgood1133@gmail.com>
2021-06-21 16:23:41 -0400

    ✔️ - wrote a function that confirms an mp3 is at "mp3_path"

--- lib/get_dictionary.py
+++ lib/get_dictionary.py
@@ -2,13 +2,14 @@
 # processes json from the dictionary api
 
 # dependencies
-import os, sys, requests, json
+import os, sys, requests
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # adds project dir to places it looks for the modules
 sys.path.append(BASE_PATH)
 
 from lib.config import staging
 from lib.backup_dict import hard_code_dict
+from lib.text_to_speech import Reader
 
 class Librarian():
     def __init__(self, word, ENV, use_test_dir=False):
@@ -49,7 +50,6 @@ class Librarian():
                 if link_for_mp3 != "":
                     res = requests.get(link_for_mp3)
                     if res.status_code == 200:
-                        # NOTE  
                         # write binary to a file in data/sounds
                         filename = my_word_dict["word"] + '.mp3'
                         file_path = self.base_dir + filename
@@ -163,7 +163,7 @@ class Librarian():
     
     def hard_coded_definitions(self):
         """get the words definition from a txt file"""
-        print('i ran ++++++++++')
+        # TODO del me print('i ran ++++++++++')
         # re-set self.data list
         self.data_list = []
         # loop through each word until we find our word
@@ -180,10 +180,33 @@ class Librarian():
         mp3_path = self.data_list[0]["mp3_path"]
 
         if not os.path.exists(mp3_path):
-            pass
             # here we are going to use tts to make the mp3 path exist 
-            # remember you still need to import the tts module you made
-            #TODO make unit tests for this file 
+            #TODO make unit tests for this file
+
+            # make a new data_list to store and then save the new dictionaries
+            new_data_list = []
+
+            # init the reader, change its base dir to this classes base dir
+            reader = Reader()
+            reader.base_dir = self.base_dir
+
+            # save the word as an mp3 for each dict in data list
+            for data_dict in self.data_list:
+                # get the word and create filename and file_path
+                word = data_dict["word"]
+                filename = (word.replace(" ","_")) + ".mp3"
+                file_path = self.base_dir + filename
+
+                # create the file
+                reader.text_to_speech(word)
+
+                # update and save the dictionary
+                data_dict['mp3_path'] = file_path
+                new_data_list.append(data_dict)
+
+
+            # update the data list with the new dictionaries
+            self.data_list = new_data_list
 
     def purge_all_mp3_files(self):
         """remove all files from self.base_dir"""
@@ -196,13 +219,13 @@ class Librarian():
                 print(f"could not remove {full_file_path}")
 
 if __name__ == "__main__":
-    #librar_bad = Librarian("abed",staging,use_test_dir=True)
+    librar_bad = Librarian("abed",staging,use_test_dir=True)
 
-    #print(librar_bad.data_list)
+    # print(librar_bad.data_list)
 
     librar_good = Librarian("wane",staging,use_test_dir=True)
 
-    print(librar_good.data_list[0])
-    print('\n')
-    print(librar_good.data_list[1])
+    #print(librar_good.data_list[0])
+    #print('\n')
+    #print(librar_good.data_list[1])
 
